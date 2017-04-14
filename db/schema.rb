0da80df3f6d5a4803a9c3e13a676f7431d3cb61e
Amit Choudhary <amitchoudhary1008@gmail.com>
2017-04-15 04:34:42 +0530

    add favourite column to api_responses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170413132320) do
+ActiveRecord::Schema.define(version: 20170414225141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20170413132320) do
     t.string "password"
     t.text "request_body"
     t.bigint "user_id"
+    t.boolean "favourite", default: false
     t.index ["token"], name: "index_api_responses_on_token", unique: true
     t.index ["user_id"], name: "index_api_responses_on_user_id"
   end
